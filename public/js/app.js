60e05bb101018ff1867c66a1dcb274b7d916ac14
Serendipity <paulalikebook@gmail.com>
2020-10-09 13:00:35 +0200

    Module JS course on Pluralsight

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,35 @@
+import getSessions, {sessionTemplate, sessionURL} from './sessionRepository.js'; // this is a default import so it does not really matter what we call this eg could be import jon from....
+import * as template from './template.js'; //or sessionTemplate as template 
 
-(function(){
-
- 
-  function render() {
+function render() {
     var list = document.querySelector('#sessions');
     if (!list) return;
     list.innerHTML = sessionTemplate(data.listItems);
-  };
-  
-  
-  var data = {
+};
+
+
+var data = {
     listItems: []
-  };
-  
-  getSessions()
-    .then((sessions)=>{
-      console.log('promises!')
-      data.listItems = sessions;
-      render();
+};
+
+getSessions()
+    .then((sessions) => {
+        console.log('promises!')
+        data.listItems = sessions;
+        render();
     });
-  
-  
-}())
-   
+
+/*
+because default can also be like
+
+import jon from './sessionRepository.js';
+
+and then
+
+jon()
+    .then((sessions) => {
+        console.log('promises!')
+        data.listItems = sessions;
+        render();
+    });
+*/
--- public/js/sessionRepository.js
+++ public/js/sessionRepository.js
@@ -2,24 +2,29 @@ const sessionURL = 'sessions.json';
 
 let sessionList = [];
 
-export default function getSessions(){
-    return new Promise(function(resolve, reject){
-      var oReq = new XMLHttpRequest();
-      oReq.onload = function (e) {
-        sessionList = e.target.response
-        resolve(sessionList);
-      };
-      oReq.open('GET', sessionURL , true);
-      oReq.responseType = 'json';
-      oReq.send();
+export default function getSessions() {
+    return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function (e) {
+            sessionList = e.target.response
+            resolve(sessionList);
+        };
+        oReq.open('GET', sessionURL, true);
+        oReq.responseType = 'json';
+        oReq.send();
     })
-  }
+}
 
 // Deafault exports
-  export {getSessions as default, sessionURL}; // default as it is not important to understand what is happening in these files -- just give it..
-  
-  // Aggregating Modules
-  export {sessionTemplate} from './template.js'; // allows what get's imported into app.js very minimum. Will just have to import one thing into app.js 
+export {getSessions as default , sessionURL };
+
+
+// default as it is not important to understand what is happening in these files -- just give it..
+
+// Aggregating Modules
+export { sessionTemplate } from './template.js';
+
+// allows what get's imported into app.js very minimum. Will just have to import one thing into app.js 
 
   //export {getSessions};
 
